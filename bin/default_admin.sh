0598241d462a4255c77c7ca8df335d92a0f82d34
Goran Mekić <meka@tilda.center>
2018-07-03 22:06:57 +0200

    Start using flask_security

---
+++ bin/default_admin.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export FLASK_ENV=development
+BIN_DIR=`dirname $0`
+PROJECT_ROOT=`readlink -f "${BIN_DIR}/.."`
+VIRTUALENV=${VIRTUALENV:="backend"}
+
+. ~/.virtualenvs/${VIRTUALENV}/bin/activate
+cd ${PROJECT_ROOT}
+flask users create -a --password Sekrit admin@example.com
+flask roles create -d Administrators admin
+flask roles add admin@example.com admin
