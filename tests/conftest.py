18d06b7dd6960e7c971664691b27f1557f4875ba
Goran Mekić <meka@tilda.center>
2019-12-14 04:53:12 +0100

    Simplify config for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,13 @@
 import os
-from importlib import import_module
 
 import pytest
 from config import configs
-from name import app_name
+from freenit import create_app
 from peewee_migrate import Router
 from peewee_migrate.router import DEFAULT_MIGRATE_DIR
 from pytest_factoryboy import register
 
 from .factories import AdminFactory, RoleFactory, UserFactory
-
-application = import_module(f'{app_name}')
 
 register(UserFactory)
 register(AdminFactory)
@@ -20,7 +17,7 @@ register(RoleFactory)
 @pytest.fixture
 def app():
     config = configs['testing']
-    flask_app = application.create_app(config)
+    flask_app = create_app(config)
     router = Router(
         flask_app.db.database,
         migrate_dir=f'{DEFAULT_MIGRATE_DIR}/main',
