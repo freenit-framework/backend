ac18d7871b9e7a2771363dcb363d0d4acae3d0b2
Goran Mekić <meka@tilda.center>
2020-02-25 20:55:49 +0100

    Initialize API

--- bin/freenit.sh
+++ bin/freenit.sh
@@ -8,7 +8,7 @@ if [ -z "${NAME}" ]; then
 fi
 
 
-MODULES="api schemas"
+MODULES="schemas"
 PROJECT_ROOT=`python -c 'import os; import freenit; print(os.path.dirname(os.path.abspath(freenit.__file__)))'`
 
 
@@ -20,6 +20,7 @@ done
 touch ${NAME}/__init__.py
 echo 'freenit' >requirements.txt
 cp -r ${PROJECT_ROOT}/project/* .
+mv api ${NAME}
 mv models ${NAME}
 echo "app_name=\"${NAME}\"  # noqa: E225" >name.py
 echo "ipdb" >requirements_dev.txt
