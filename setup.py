adba8df00257ac6bdfb96dd17039023ce32e128a
Goran Mekić <meka@tilda.center>
2020-03-30 02:13:13 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ sql = [
 
 setup(
     name='freenit',
-    version='0.1.5',
+    version='0.1.6',
     description='REST API framework based on Flask-Smorest',
     long_description=README,
     long_description_content_type='text/markdown',
