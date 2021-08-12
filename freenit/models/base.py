3af96648674aba86b5224f8240b8a894d57bff70
Goran Mekić <meka@tilda.center>
2021-08-12 12:53:53 +0200

    Include alembic.ini

--- freenit/models/base.py
+++ freenit/models/base.py
@@ -1,5 +1,5 @@
 class ModelsBase:
-    def __init__(self, base, response, listing):
+    def __init__(self, base, response=None, listing=None):
         self.base = base
-        self.response = response
-        self.listing = listing
+        self.response = response if response is not None else base
+        self.listing = listing if listing is not None else base
