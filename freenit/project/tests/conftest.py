b5df4ab4f48afbe017d83ce061faa1433d2087a7
Goran Mekić <meka@tilda.center>
2021-10-29 10:56:27 +0200

    Make test config more general

--- freenit/project/tests/conftest.py
+++ freenit/project/tests/conftest.py
@@ -1,22 +1,26 @@
+import importlib
 import os
+
 import pytest
 from pytest_factoryboy import register
+
+from alembic import command
+from alembic.config import Config
+from name import app_name
+
 from .factories import UserFactory
 
-
+alembic_cfg = Config("alembic.ini")
 register(UserFactory)
 
 
 @pytest.fixture
 def db_setup():
-    from freenit.app import app as fastapp
-    from alembic.config import Config
-    from alembic import command
+    app = importlib.import_module(f"{app_name}.app")
 
-    alembic_cfg = Config("alembic.ini")
     command.upgrade(alembic_cfg, "head")
 
-    yield fastapp
+    yield app.app
 
     current_path = os.path.dirname(__file__)
-    os.remove(f'{current_path}/../test.sqlite')
+    os.remove(f"{current_path}/../test.sqlite")
