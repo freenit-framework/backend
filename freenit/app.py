05dfb1cce5afb139ca03e4a66db1fed1ecd6670f
Goran Mekić <meka@tilda.center>
2022-02-12 21:08:51 +0100

    Database connect/disconnect

--- freenit/app.py
+++ freenit/app.py
@@ -5,4 +5,18 @@ from .config import getConfig
 
 config = getConfig()
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    if not config.database.is_connected:
+        await config.database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    if config.database.is_connected:
+        await config.database.disconnect()
+
+
 app.mount("/api/v1", api)
--- freenit/project/project/app.py
+++ freenit/project/project/app.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
-from freenit.config import getConfig
+
 from .api import api
+from .config import getConfig
 
 config = getConfig()
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    if not config.database.is_connected:
+        await config.database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    if config.database.is_connected:
+        await config.database.disconnect()
+
+
 app.mount("/api/v1", api)
